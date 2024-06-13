a16227e8cb1356468606dc4d1d888e9da8f2104c
Eric <eric.sauvageat@meteoswiss.ch>
2024-06-13 15:13:55 +0000

    merge rolf test with eric test

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -2,13 +2,20 @@ import os
 import shutil
 import unittest
 
-from dl_toolbox_runner.configure import Configurator
+import xarray as xr
+
+from dl_toolbox_runner.errors import FilenameError
+from dl_toolbox_runner.configure import Configurator, get_conf
 from dl_toolbox_runner.utils.file_utils import abs_file_path
 
 datafile = abs_file_path('dl_toolbox_runner/data/input/DWL_raw_PAYWL_2023-01-01_00-06-12_dbs_303_50mTP.nc')
 # TODO: potentially use data file present under test folder (once test_retrieval.py has been improved)
 outdir = abs_file_path('tests/tmp_test_configurator')
-configfile = os.path.join(outdir, 'PAYWL_conftest.conf')
+#configfile = os.path.join(outdir, 'PAYWL_conftest.conf')
+
+configfile = abs_file_path('dl_toolbox_runner/data/toolbox/sample_config/PAYWL_DBS_TP.conf')
+defaultconfigfile = abs_file_path('dl_toolbox_runner/config/default_config_windcube.yaml')
+mainconfigfile = abs_file_path('dl_toolbox_runner/config/main_config.yaml')
 
 
 class TestConf(unittest.TestCase):
@@ -21,9 +28,16 @@ class TestConf(unittest.TestCase):
         shutil.rmtree(outdir)
 
     def test_full(self):
-        """test for setting up the configuraiton file for the DL-toolbox run"""
-        x = Configurator(datafile, configfile)
+        """Test for the Configurator class"""
+
+        x = Configurator('PAYWL', 'DBS_TP',  datafile, configfile, main_config=get_conf(mainconfigfile), file_default_config=defaultconfigfile)
         x.run()
 
     # TODO: test outputfile against a reference file
     # TODO: test different situations
+    
+    def test_GetConf(self):
+        """Test for the get_conf function"""
+        conf = get_conf(abs_file_path('dl_toolbox_runner/config/default_config_windcube.yaml'))
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf['system'], 'windcube')
