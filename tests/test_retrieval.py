e44003f7d3c91cb319470d74475fcab70ba2f346
Eric <eric.sauvageat@meteoswiss.ch>
2024-06-20 15:15:36 +0000

    added specific config for test

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from dl_toolbox_runner.main import Runner
 from dl_toolbox_runner.utils.file_utils import abs_file_path
@@ -10,9 +11,11 @@ class TestRetrieval(unittest.TestCase):
 
     def test_full(self):
         """integration test for sample retrieval with DL toolbox"""
-        x = Runner(abs_file_path('dl_toolbox_runner/config/main_config.yaml'))
-        # TODO: use a different test config pointing to a distinct data input directory under the test directory
-        x.run()
+        x = Runner(abs_file_path('dl_toolbox_runner/config/config_test.yaml'),  single_process=True)
+        x.run(instrument_id='PAYWL')
+
+        # Check if output file has been written:
+        assert os.path.isfile(os.path.join(x.conf['output_dir'], (x.conf['output_file_prefix'] + 'PAYWL_202301010006.nc')))
 
     # TODO: test outputfile against a reference file
     # TODO: test different situations
